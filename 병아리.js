ca1e99b1fa6ac4b27ef92bb5769e046e973e40f9
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2020-06-06 20:17:53 -0700

    토론 기능

--- 병아리.js
+++ 병아리.js
@@ -6,6 +6,17 @@ function beep(cnt = 1) { // 경고음 재생
 		prt("");
 }
 
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function rndval(chars, length) {
+	var result           = '';
+	var characters       = chars;
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
+
 const timeFormat = 'Y-m-d H:i:s';
 
 const inputReader = require('wait-console-input'); // 입력받는 라이브러리
@@ -138,7 +149,9 @@ catch(e) {
 		'config': ['key', 'value'],
 		'email_filters': ['address'],
 		'stars': ['title', 'username', 'lastedit'],
-		'perms': ['perm', 'username']
+		'perms': ['perm', 'username'],
+		'threads': ['title', 'topic', 'status', 'time', 'tnum'],
+		'res': ['id', 'content', 'username', 'time', 'hidden', 'hider', 'status', 'tnum', 'ismember', 'isadmin']
 	};
 	
 	for(var table in tables) {
@@ -198,7 +211,7 @@ function getSkin() {
 
 async function getperm(perm, username) {
 	await curs.execute("select perm from perms where username = ? and perm = ?", [username, perm]);
-	if(curs.fetchall()) {
+	if(curs.fetchall().length) {
 		return true;
 	}
 	return false;
@@ -237,7 +250,7 @@ function render(req, title = '', content = '', varlist = {}, subtitle = '', erro
 	templateVariables['content'] = content;
 	templateVariables['perms'] = perms;
 	
-	if(['wiki', 'notfound'].includes(viewname)) {
+	if(viewname != '') {
 		templateVariables['document'] = title;
 	}
 	
@@ -471,8 +484,8 @@ wiki.get(/^\/edit\/(.*)/, async function(req, res) {
 	} else {
 		content = `
 			<form method="post" id="editForm" enctype="multipart/form-data" data-title="${title}" data-recaptcha="0">
-				<input type="hidden" name="token" value="fa9585bd2f508bbd37162ebaccc6018f6609c69ac92e9c0a7e3dc7d2320c60f6">
-				<input type="hidden" name="identifier" value="i:207.241.226.232">
+				<input type="hidden" name="token" value="">
+				<input type="hidden" name="identifier" value="${islogin() ? 'm' : 'i'}:${ip_check(req)}">
 				<input type="hidden" name="baserev" value="${baserev}">
 
 				<ul class="nav nav-tabs" role="tablist" style="height: 38px;">
@@ -500,7 +513,7 @@ wiki.get(/^\/edit\/(.*)/, async function(req, res) {
 
 				<label><input type="checkbox" name="agree" id="agreeCheckbox" value="Y">&nbsp;문서 편집을 <strong>저장</strong>하면 당신은 기여한 내용을 <strong>CC-BY-NC-SA 2.0 KR</strong>으로 배포하고 기여한 문서에 대한 하이퍼링크나 URL을 이용하여 저작자 표시를 하는 것으로 충분하다는 데 동의하는 것입니다. 이 <strong>동의는 철회할 수 없습니다.</strong></label>
 				
-				<p style="font-weight: bold;">비로그인 상태로 편집합니다. 편집 역사에 IP(${req.headers['x-forwarded-for']})가 영구히 기록됩니다.</p>
+				<p style="font-weight: bold;">비로그인 상태로 편집합니다. 편집 역사에 IP(${ip_check(req)})가 영구히 기록됩니다.</p>
 				
 				<div class="btns">
 					<button id="editBtn" class="btn btn-primary" style="width: 100px;">저장</button>
@@ -584,28 +597,135 @@ wiki.post(/^\/edit\/(.*)/, async function(req, res) {
 	res.redirect('/w/' + title);
 });
 
-wiki.get(/^\/history\/(.*)/, function(req, res) {
+wiki.get('/RecentChanges', async function(req, res) {
+	var flag = req.query['logtype'];
+	if(!flag) flag = 'all';
+	
+	switch(flag) {
+		case 'create':
+			await curs.execute("select title, rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+						where advance like '(새 문서)' order by cast(rev as integer) desc limit 100");
+		break;case 'delete':
+			await curs.execute("select title, rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+						where advance like '(삭제)' order by cast(rev as integer) desc limit 100");
+		break;case 'move':
+			await curs.execute("select title, rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+						where advance like '(%이동)' order by cast(rev as integer) desc limit 100");
+		break;case 'revert':
+			await curs.execute("select title, rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+						where advance like '(%되돌림)' order by cast(rev as integer) desc limit 100");
+		break;default:
+			await curs.execute("select title, rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+						order by cast(rev as integer) desc limit 100");
+	}
+	
+	if(!curs.fetchall().length) return showError(req, 'document_dont_exists');
+	
+	const navbtns = navbtn(0, 0, 0, 0);
+	
+	var content = `
+		<ol class="breadcrumb link-nav">
+			<li><a href="?logtype=all">[전체]</a></li>
+			<li><a href="?logtype=create">[새 문서]</a></li>
+			<li><a href="?logtype=delete">[삭제]</a></li>
+			<li><a href="?logtype=move">[이동]</a></li>
+			<li><a href="?logtype=revert">[되돌림]</a></li>
+		</ol>
+		
+		<table class="table table-hover">
+			<colgroup>
+				<col>
+				<col style="width: 25%;">
+				<col style="width: 22%;">
+			</colgroup>
+			
+			<thead id>
+				<tr>
+					<th>항목</th>
+					<th>수정자</th>
+					<th>수정 시간</th>
+				</tr>
+			</thead>
+			
+			<tbody id>
+	`;
+	
+	for(row of curs.fetchall()) {
+		content += `
+				<tr${(row['log'].length > 0 || row['advance'].length > 0 ? ' class=no-line' : '')}>
+					<td>
+						<a href="/w/${encodeURI(row['title'])}">${html.escape(row['title'])}</a> 
+						<a href="/history/${encodeURI(row['title'])}">[역사]</a> 
+						${
+								Number(row['rev']) > 1
+								? '<a \href="/diff/' + encodeURI(row['title']) + '?rev=' + row['rev'] + '&oldrev=' + String(Number(row['rev']) - 1) + '">[비교]</a>'
+								: ''
+						} 
+						<a href="/discuss/${encodeURI(row['title'])}">[토론]</a> 
+						
+						(<span style="color: ${
+							(
+								Number(row['changes']) > 0
+								? 'green'
+								: (
+									Number(row['changes']) < 0
+									? 'red'
+									: 'gray'
+								)
+							)
+							
+						};">${row['changes']}</span>)
+					</td>
+					
+					<td>
+						${ip_pas(row['username'], row['ismember'])}
+					</td>
+					
+					<td>
+						${generateTime(toDate(row['time']), timeFormat)}
+					</td>
+				</tr>
+		`;
+		
+		if(row['log'].length > 0) {
+			content += `
+				<td colspan="3" style="padding-left: 1.5rem;">
+					${row['log']} <i>${row['advance']}</i>
+				</td>
+			`;
+		}
+	}
+	
+	content += `
+			</tbody>
+		</table>
+	`;
+	
+	res.send(render(req, '최근 변경내역', content, {}));
+});
+
+wiki.get(/^\/history\/(.*)/, async function(req, res) {
 	const title = req.params[0];
 	const from = req.query['from'];
 	const until = req.query['until'];
 	
 	if(from) { // 더시드에서 from이 더 우선임
-		curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+		await curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
 						where title = ? and (cast(rev as integer) <= ? AND cast(rev as integer) > ?) \
 						order by cast(rev as integer) desc",
-						[title, from, String(Number(from) - 30)]);
+						[title, Number(from), Number(from) - 30]);
 	} else if(until) {
-		curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+		await curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
 						where title = ? and (cast(rev as integer) >= ? AND cast(rev as integer) < ?) \
 						order by cast(rev as integer) desc",
-						[title, from, String(Number(until) + 30)]);
+						[title, Number(until), Number(until) + 30]);
 	} else {
-		curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
+		await curs.execute("select rev, time, changes, log, iserq, erqnum, advance, ismember, username from history \
 						where title = ? order by cast(rev as integer) desc limit 30",
 						[title]);
 	}
 	
-	if(!curs.fetchall().length) return showError(req, 'document_dont_exists');
+	if(!curs.fetchall().length) res.send(showError(req, 'document_dont_exists'));
 	
 	const navbtns = navbtn(0, 0, 0, 0);
 	
@@ -670,7 +790,341 @@ wiki.get(/^\/history\/(.*)/, function(req, res) {
 		<script>historyInit("${encodeURI(title)}");</script>
 	`;
 	
-	res.send(render(req, title, content, {}, '의 역사', error = false, viewname = 'history'));
+	res.send(render(req, title, content, _, '의 역사', error = false, viewname = 'history'));
+});
+
+wiki.get(/^\/discuss\/(.*)/, async function(req, res) {
+	const title = req.params[0];
+	
+	var state = req.query['state'];
+	if(!state) state = '';
+	
+	var content = '';
+	
+	var trdlst;
+	
+	var subtitle = '';
+	var viewname = '';
+	
+	switch(state) {
+		case 'close':
+			content += '<ul class=wiki-list>';
+			
+			var cnt = 0;
+			await curs.execute("select topic, tnum from threads where title = ? and status = 'close' order by cast(time as integer) desc");
+			trdlst = curs.fetchall();
+			
+			for(trd of trdlst) {
+				content += `<li><a href="#${++cnt}">${cnt}</a>. <a href="/thread/${trd['tnum']}">${html.escape(trd['topic'])}</a></li>`;
+			}
+			
+			content += '</ul>';
+			
+			subtitle = ' (닫힌 토론)';
+			
+			viewname = 'thread_list_close'
+		break;default:
+			content += `
+				<h3 class="wiki-heading">편집 요청</h3>
+				<div class=wiki-heading-content>
+					<ul class=wiki-list>
+			`;
+			
+			content += `
+					</ul>
+				</div>
+				
+				<p>
+					<a href="?state=closed_edit_requests">[닫힌 편집 요청 보기]</a>
+				</p>
+			`;
+			
+			content += `
+				<h3 class="wiki-heading">토론</h3>
+				<div class=wiki-heading-content>
+					<ul class=wiki-list>
+			`;
+			
+			var cnt = 0;
+			await curs.execute("select topic, tnum from threads where title = ? and status = 'normal' order by cast(time as integer) desc", [title]);
+			trdlst = curs.fetchall();
+			
+			for(trd of trdlst) {
+				content += `<li><a href="#${++cnt}">${cnt}</a>. <a href="/thread/${trd['tnum']}">${html.escape(trd['topic'])}</a></li>`;
+			}
+			
+			content += `
+					</ul>
+				</div>
+					
+				<p>
+					<a href="?state=close">[닫힌 토론 목록 보기]</a>
+				</p>
+				
+				<h3 class="wiki-heading">새 주제 생성</h3>
+				
+				<form method="post" class="new-thread-form" id="topicForm">
+					<input type="hidden" name="identifier" value="${islogin(req) ? 'm' : 'i'}:${ip_check(req)}">
+					<div class="form-group">
+						<label class="control-label" for="topicInput" style="margin-bottom: 0.2rem;">주제 :</label>
+						<input type="text" class="form-control" id="topicInput" name="topic">
+					</div>
+
+					<div class="form-group">
+					<label class="control-label" for="contentInput" style="margin-bottom: 0.2rem;">내용 :</label>
+						<textarea name="text" class="form-control" id="contentInput" rows="5"></textarea>
+					</div>
+
+					
+					<p style="font-weight: bold; font-size: 1rem;">[알림] 비로그인 상태로 토론 주제를 생성합니다. 토론 내역에 IP(${ip_check(req)})가 영구히 기록됩니다.</p>
+					
+
+					<div class="btns">
+						<button id="createBtn" class="btn btn-primary" style="width: 8rem;">전송</button>
+					</div>
+
+					<!--
+					<div id="recaptcha"><div><noscript>Aktiviere JavaScript, um eine reCAPTCHA-Aufgabe zu erhalten.&lt;br&gt;</noscript><div class="if-js-enabled">Führe ein Upgrade auf einen <a href="http://web.archive.org/web/20171027095753/https://support.google.com/recaptcha/?hl=en#6223828">unterstützten Browser</a> aus, um eine reCAPTCHA-Aufgabe zu erhalten.</div><br>Wenn du meinst, dass diese Seite fälschlicherweise angezeigt wird, überprüfe bitte deine Internetverbindung und lade die Seite neu.<br><br><a href="http://web.archive.org/web/20171027095753/https://support.google.com/recaptcha#6262736" target="_blank">Warum gerade ich?</a></div></div>
+					<script>
+						recaptchaInit('recaptcha', {
+							'sitekey': '6LcUuigTAAAAALyrWQPfwtFdFWFdeUoToQyVnD8Y',
+							'size': 'invisible',
+							'bind': 'createBtn',
+							'badge': 'inline',
+							'callback': function() { $("#createBtn").attr("disabled", true); $("#topicForm").submit(); }
+						});
+					</script>
+					-->
+				</form>
+			`;
+			
+			subtitle = ' (토론)';
+			viewname = 'thread_list'
+	}
+	
+	res.send(render(req, title, content, _, subtitle, false, viewname));
+});
+
+wiki.post(/^\/discuss\/(.*)/, async function(req, res) {
+	const title = req.params[0];
+	
+	if(!getacl(title, 'create_thread')) res.send(showError(req, 'insufficient_privileges'));
+	
+	var tnum = rndval('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 22);
+	
+	while(1) {
+		await curs.execute("select tnum from threads where tnum = ?", [tnum]);
+		if(!curs.fetchall().length) break;
+		tnum = rndval('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 22);
+	}
+	
+	curs.execute("insert into threads (title, topic, status, time, tnum) values (?, ?, ?, ?, ?)",
+					[title, req.body['topic'], 'normal', getTime(), tnum]);
+	
+	curs.execute("insert into res (id, content, username, time, hidden, hider, status, tnum, ismember, isadmin) values \
+					(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+					['1', req.body['text'], ip_check(req), getTime(), '0', '', '0', tnum, islogin(req) ? 'author' : 'ip', getperm('admin', ip_check(req)) ? '1' : '0']);
+					
+	res.redirect('/thread/' + tnum);
+});
+
+wiki.get('/thread/:tnum', async function(req, res) {
+	const tnum = req.param("tnum");
+	
+	await curs.execute("select id from res where tnum = ?", [tnum]);
+	
+	const rescount = curs.fetchall().length;
+	
+	if(!rescount) res.send(showError("thread_not_found"));
+	
+	await curs.execute("select title, topic, status from threads where tnum = ?", [tnum]);
+	const title = curs.fetchall()[0]['title'];
+	const topic = curs.fetchall()[0]['topic'];
+	const status = curs.fetchall()[0]['status'];
+	
+	if(!getacl(title, 'read')) res.send(showError(req, 'insufficient_privileges_read'));
+	
+	var content = `
+		<h2 class=wiki-heading style="cursor: pointer;">${html.escape(topic)}</h2>
+		<div class=wiki-heading-content>
+		
+			<div id=res-container>
+	`;
+	
+	for(var i=1; i<=rescount; i++) {
+		content += `
+			<div class="res-wrapper res-loading" data-id="${i}" data-locked="false" data-visible=false>
+				<div class="res res-type-normal">
+					<div class="r-head">
+						<span class="num"><a id="${i}">#${i}</a>&nbsp;</span>
+					</div>
+					
+					<div class="r-body"></div>
+				</div>
+			</div>
+		`;
+	}
+	
+	content += `
+			</div>
+		</div>
+		
+		<h2 class=wiki-heading style="cursor: pointer;">댓글 달기</h2>
+	`;
+	
+	if(getperm('update_thread_status', ip_check(req))) {
+		var sts = '';
+		
+		switch(status) {
+			case 'close':
+				sts = `
+					<option value="normal">normal</option>
+					<option value="pause">pause</option>
+				`;
+			break;case 'normal':
+				sts = `
+					<option value="close">close</option>
+					<option value="pause">pause</option>
+				`;
+			break;case 'pause':
+				sts = `
+					<option value="close">close</option>
+					<option value="normal">normal</option>
+				`;
+		}
+		
+		content += `
+		    <form method="post" id="thread-status-form">
+        		[ADMIN] 쓰레드 상태 변경
+        		<select name="status">${sts}</select>
+        		<button id="changeBtn" class="d_btn type_blue">변경</button>
+        	</form>
+		`;
+	}
+	
+	if(getperm('update_thread_document', ip_check(req))) {
+		content += `
+        	<form method="post" id="thread-document-form">
+        		[ADMIN] 쓰레드 이동
+        		<input type="text" name="document" value="${title}">
+        		<button id="changeBtn" class="d_btn type_blue">변경</button>
+        	</form>
+		`;
+	}
+	
+	if(getperm('update_thread_topic', ip_check(req))) {
+		content += `
+        	<form method="post" id="thread-topic-form">
+        		[ADMIN] 쓰레드 주제 변경
+        		<input type="text" name="topic" value="${topic}">
+        		<button id="changeBtn" class="d_btn type_blue">변경</button>
+        	</form>
+		`;
+	}
+	
+	content += `
+		<script>$(function() { discussPollStart("${tnum}"); });</script>
+	
+		<form id=new-thread-form method=post>
+			<textarea class=form-control rows=5 name=text ${['close', 'pause'].includes(status) ? 'disabled' : ''}>${status == 'pause' ? 'pause 상태입니다.' : (status == 'close' ? '닫힌 토론입니다.' : '')}</textarea>
+		
+			<div class=btns>
+				<button type=submit class="btn btn-primary" style="width: 120px;">전송</button>
+			</div>
+		</form>
+	`;
+	
+	res.send(render(req, title, content, {}, ' (토론) - ' + topic, error = false, viewname = 'thread'));
+});
+
+wiki.post('/thread/:tnum', async function(req, res) {
+	const tnum = req.param("tnum");
+	
+	await curs.execute("select id from res where tnum = ?", [tnum]);
+	
+	const rescount = curs.fetchall().length;
+	
+	if(!rescount) res.send(showError("thread_not_found"));
+	
+	await curs.execute("select title, topic, status from threads where tnum = ?", [tnum]);
+	const title = curs.fetchall()[0]['title'];
+	const topic = curs.fetchall()[0]['topic'];
+	const status = curs.fetchall()[0]['status'];
+	
+	if(!getacl(title, 'write_thread_comment')) res.send(showError(req, 'insufficient_privileges'));
+	
+	await curs.execute("select id from res where tnum = ? order by cast(id as integer) desc limit 1", [tnum]);
+	const lid = Number(curs.fetchall()[0]['id']);
+	
+	curs.execute("insert into res (id, content, username, time, hidden, hider, status, tnum, ismember, isadmin) \
+					values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+						String(lid + 1), req.body['text'], ip_check(req), getTime(), '0', '', '0', tnum, islogin(req) ? 'author' : 'ip', getperm('admin', ip_check(req)) ? '1' : '0'
+					]);
+					
+	curs.execute("update threads set time = ? where tnum = ?", [getTime(), tnum]);
+	
+	res.json({});
+});
+
+wiki.get('/thread/:tnum/:id', async function(req, res) {
+	const tnum = req.param("tnum");
+	const tid = req.param("id");
+	
+	await curs.execute("select id from res where tnum = ?", [tnum]);
+	
+	const rescount = curs.fetchall().length;
+	
+	if(!rescount) res.send(showError("thread_not_found"));
+	
+	await curs.execute("select username from res where tnum = ? and (id = '1')", [tnum]);
+	const fstusr = curs.fetchall()[0]['username'];
+	
+	await curs.execute("select title, topic, status from threads where tnum = ?", [tnum]);
+	const title = curs.fetchall()[0]['title'];
+	const topic = curs.fetchall()[0]['topic'];
+	const status = curs.fetchall()[0]['status'];
+	
+	if(!getacl(title, 'read')) res.send(showError(req, 'insufficient_privileges_read'));
+	
+	content = ``;
+	
+	await curs.execute("select id, content, username, time, hidden, hider, status, ismember from res where tnum = ? and (cast(id as integer) = 1 or (cast(id as integer) >= ? and cast(id as integer) < ?)) order by cast(id as integer) asc", [tnum, Number(tid), Number(tid) + 30]);
+	for(rs of curs.fetchall()) {
+		content += `
+			<div class=res-wrapper data-id="${rs['id']}">
+				<div class="res res-type-${rs['status'] == '1' ? 'status' : 'normal'}">
+					<div class="r-head${rs['username'] == fstusr ? " first-author" : ''}">
+						<span class=num>
+							<a id="${rs['id']}">#${rs['id']}</a>&nbsp;
+						</span> ${ip_pas(rs['username'])} <span style="float: right;">${generateTime(toDate(rs['time']), timeFormat)}</span>
+					</div>
+					
+					<div class="r-body${rs['hidden'] == '1' ? ' r-hidden-body' : ''}">
+						${markdown(rs['content'], rs['ismember'])}
+					</div>
+				</div>
+			</div>
+		`;
+	}
+	
+	res.send(content);
+});
+
+wiki.post('/notify/thread/:tnum', async function(req, res) {
+	var tnum = req.param("tnum");
+	
+	await curs.execute("select id from res where tnum = ?", [tnum]);
+	
+	const rescount = curs.fetchall().length;
+	
+	if(!rescount) res.send(showError("thread_not_found"));
+	
+	await curs.execute("select id from res where tnum = ? order by cast(time as integer) desc limit 1", [tnum]);
+	
+	res.json({
+		"status": "event",
+		"comment_id": Number(curs.fetchall()[0]['id'])
+	});
 });
 
 wiki.use(function(req, res, next) {
