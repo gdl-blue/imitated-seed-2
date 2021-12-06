a1b7321d572488985e5ab8b617f44872e847c4c2
gdl-888 <55873929+gdl-888@users.noreply.github.com>
2021-12-06 11:04:46 +0900

    편집 요청 UI 구현

--- index.js
+++ index.js
@@ -127,17 +127,17 @@ wiki.use(session({
 	}
 }));
 
-var ipRangeCheck = require("ip-range-check");
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+const ipRangeCheck = require("ip-range-check");
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const diff = require('./cemerick-jsdifflib.js');
+const upload = multer();
+const fs = require('fs');
 
 wiki.use(bodyParser.json());
 wiki.use(bodyParser.urlencoded({ extended: true }));
 wiki.use(upload.array()); 
 wiki.use(express.static('public'));
-
-const fs = require('fs');
 
 var wikiconfig = {};
 var permlist = {};
@@ -148,16 +148,12 @@ try {
 	hostconfig = require('./config.json'); 
 	_ready = 1; 
 } catch(e) { (async function() {
-	print("병아리 엔진: the seed 모방 프로젝트에 오신것을 환영합니다.");
-	print("버전 4.5.5 [디버그] - 테스트 목적으로만 사용됩니다.");
-	print("고의적으로 배포하지 마십시오.");
-	
-	prt('\n');
+	print("병아리 - the seed 모방 엔진에 오신것을 환영합니다.\n");
 	
 	hostconfig = {
 		host: input("호스트 주소: "),
 		port: input("포트 번호: "),
-		skin: input("기본 스킨 이름: ")
+		skin: input("기본 스킨 이름: "),
 	};
 	
 	const tables = {
@@ -183,6 +179,7 @@ try {
 		'aclgroup_groups': ['name', 'admin', 'date', 'lastupdate'],
 		'aclgroup': ['aclgroup', 'type', 'username', 'note', 'date', 'expiration', 'id'],
 		'block_history': ['date', 'type', 'aclgroup', 'id', 'duration', 'note', 'executer', 'target'],
+		'edit_requests': ['title', 'namespace', 'id', 'deleted', 'state', 'content', 'baserev', 'username', 'ismember', 'log', 'date', 'processor', 'processortype', 'lastupdate', 'processtime', 'reason', 'rev'],
 	};
 	
 	for(var table in tables) {
@@ -373,6 +370,7 @@ function fetchErrorString(code) {
 		insufficient_privileges: '권한이 부족합니다.',
 		thread_not_found: '토론이 존재하지 않습니다.',
 		invalid_signup_key: '인증 요청이 만료되었거나 올바르지 않습니다.',
+		document_not_found: '문서를 찾을 수 없습니다.',
 	};
 	
 	if(typeof(codes[code]) == 'undefined') return code;
@@ -891,7 +889,6 @@ wiki.get(/^\/edit\/(.*)/, async function editDocument(req, res) {
 	}
 	
 	var rawContent = await curs.execute("select content from documents where title = ? and namespace = ?", [doc.title, doc.namespace]);
-	
 	if(!rawContent[0]) rawContent = '';
 	else rawContent = rawContent[0].content;
 	
@@ -899,7 +896,6 @@ wiki.get(/^\/edit\/(.*)/, async function editDocument(req, res) {
 	var content = '';
 	
 	var baserev;
-	
 	var data = await curs.execute("select rev from history where title = ? and namespace = ? order by CAST(rev AS INTEGER) desc limit 1", [doc.title, doc.namespace]);
 	try {
 		baserev = data[0].rev;
@@ -1053,15 +1049,206 @@ wiki.post(/^\/preview\/(.*)$/, async(req, res) => {
 	`);
 });
 
-wiki.get(/^\/new_edit_request\/(.*)$/, async(req, res) => {
+wiki.get(/^\/edit_request\/(\d+)\/preview$/, async(req, res, next) => {
+	const id = req.params[0];
+	var data = await curs.execute("select title, namespace, state, content, baserev, username, ismember, log, date, processor, processortype, processtime, lastupdate, reason, rev from edit_requests where not deleted = '1' and id = ?", [id]);
+	if(!data.length) return res.send(showError(req, 'edit_request_not_found'));
+	const item = data[0];
+	const doc = totitle(item.title, item.namespace);
+	
+	return res.send(`
+		<head>
+			<meta charset=utf8 />
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+			<link rel="stylesheet" href="/css/diffview.css">
+			<link rel="stylesheet" href="/css/katex.min.css">
+			<link rel="stylesheet" href="/css/wiki.css">
+			<!--[if (!IE)|(gt IE 8)]><!--><script type="text/javascript" src="/js/jquery-2.1.4.min.js"></script><!--<![endif]-->
+			<!--[if lt IE 9]><script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script><![endif]-->
+			<script type="text/javascript" src="/js/dateformatter.js?508d6dd4"></script>
+			<script type="text/javascript" src="/js/intersection-observer.js?36e469ff"></script>
+			<script type="text/javascript" src="/js/theseed.js?24141115"></script>
+		</head>
+		
+		<body>
+			<h1>${html.escape(doc + '')}</h1>
+			${await markdown(item.content, 0, doc + '')}
+		</body>
+	`);
+});
+
+wiki.get(/^\/edit_request\/(\d+)$/, async(req, res, next) => {
+	const id = req.params[0];
+	// 'edit_requests': ['title', 'namespace', 'id', 'deleted', 'state', 'content', 'baserev', 'username', 'ismember', 'log', 'date', 'processor', 'processortype', 'lastupdate'],
+	var data = await curs.execute("select title, namespace, state, content, baserev, username, ismember, log, date, processor, processortype, processtime, lastupdate, reason, rev from edit_requests where not deleted = '1' and id = ?", [id]);
+	if(!data.length) return res.send(showError(req, 'edit_request_not_found'));
+	const item = data[0];
+	const doc = totitle(item.title, item.namespace);
+	var data = await curs.execute("select content from history where title = ? and namespace = ? and rev = ?", [item.title, item.namespace, item.baserev]);
+	var base = '';
+	if(data.length) base = data[0].content;
+	
+	var card = '';
+	switch(item.state) {
+		case 'open': {
+			const acceptable = await getacl(req, item.title, item.namespace, 'edit');
+			const closable   = hasperm(req, 'update_thread_status') || ((islogin(req) ? 'author' : 'ip') == item.ismember && item.username == ip_check(req));
+			const editable   = ((islogin(req) ? 'author' : 'ip') == item.ismember && item.username == ip_check(req));
+			
+			card = `
+				<h4 class="card-title">이 편집 요청을...</h4>
+				<p class="card-text">${generateTime(toDate(item.lastupdate), timeFormat)}에 마지막으로 수정됨</p>
+				<form id="edit-request-accept-form" action="/edit_request/${id}/accept" method=post style="display: inline;">
+					<button${acceptable ? '' : ' disabled'} class="btn btn-lg btn-success${acceptable ? '' : ' disabled'}" data-toggle="tooltip" data-placement="top" title="${acceptable ? '이 편집 요청을 문서에 적용합니다.' : '이 문서를 편집할 수 있는 권한이 없습니다.'}" type="submit">Accept</button>
+				</form>
+				<span data-toggle="modal" data-target="#edit-request-close-modal">
+					<button${closable ? '' : ' disabled'} class="btn btn-lg${closable ? '' : ' disabled'}" data-toggle="tooltip" data-placement="top" title="${closable ? '이 편집 요청을 닫습니다.' : '편집 요청을 닫기 위해서는 요청자 본인이거나 권한이 있어야 합니다.'}" type="button">Close</button>
+				</span>
+				<a class="btn btn-info btn-lg${editable ? '' : ' disabled'}" data-toggle="tooltip" data-placement="top" title="${editable ? '이 편집 요청을 수정합니다.' : '요청자 본인만 수정할 수 있습니다.'}" href="/edit_request/${id}/edit">Edit</a>
+			`;
+		} break; case 'closed': {
+			card = `
+				<h4 class="card-title">편집 요청이 닫혔습니다.</h4>
+				<p class="card-text">${generateTime(toDate(item.processtime), timeFormat)}에 ${ip_pas(item.processor, item.processortype)}가 편집 요청을 닫았습니다.</p>
+				${item.reason ? `<p class="card-text">사유 : ${html.escape(item.reason)}</p>` : ''}
+			`;
+		} break; case 'accepted': {
+			card = `
+				<h4 class="card-title">편집 요청이 승인되었습니다.</h4>
+				<p class="card-text">${generateTime(toDate(item.processtime), timeFormat)}에 ${ip_pas(item.processor, item.processortype)}가 r${item.rev}으로 승인함.</p>
+			`;
+		}
+	}
+	
+	var content = `
+		<h3> ${ip_pas(item.username, item.ismember)}가 ${generateTime(toDate(item.date), timeFormat)}에 요청</h3>
+		<hr />
+		<div class="form-group">
+			<label class="control-label">기준 판</label> r${item.baserev}
+		</div>
+		
+		<div class="form-group">
+			<label class="control-label">편집 요약</label> ${html.escape(item.log)}
+		</div>
+		
+		${item.state == 'open' ? `
+			<div id="edit-request-close-modal" class="modal fade" role="dialog" style="display: none;" aria-hidden="true">
+				<div class="modal-dialog">
+					<form id="edit-request-close-form" method="post" action="/edit_request/${id}/close">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">×</button> 
+								<h4 class="modal-title" style="border:none">편집 요청 닫기</h4>
+							</div>
+							<div class="modal-body">
+								<p>사유:</p>
+								<input name="close_reason" type="text"> 
+							</div>
+							<div class="modal-footer"> <button type="submit" class="btn btn-primary" style="width:auto">확인</button> <button type="button" class="btn btn-default" data-dismiss="modal" style="background:#efefef">취소</button> </div>
+						</div>
+					</form>
+				</div>
+			</div>
+		` : ''}
+		
+		<div class="card">
+			<div class="card-block">
+				${card}
+			</div>
+		</div>
+		
+		<br />
+		
+		${diff(base, item.content, '1', '2').replace('<th class="texttitle">1 vs. 2</th>', '<th class="texttitle"><a target=_blank href="/edit_request/' + id + '/preview">(미리보기)</a></th>')}
+	`;
+	
+	var error = false;
+	
+	return res.send(render(req, doc + ' (편집 요청 ' + id + ')', content, {
+		document: doc,
+	}, _, error, 'edit_request'));
+});
+
+wiki.all(/^\/new_edit_request\/(.*)$/, async(req, res, next) => {
+	if(!['POST', 'GET'].includes(req.method)) return next();
+	
 	const title = req.params[0];
 	const doc = processTitle(title);
 	
+	var data = await curs.execute("select title from documents \
+					where title = ? and namespace = ?",
+					[doc.title, doc.namespace]);
+	if(!data.length) res.send(showError(req, 'document_not_found'));
+	
 	const aclmsg = await getacl(req, doc.title, doc.namespace, 'edit_request', 1);
 	if(aclmsg) return res.send(showError(req, aclmsg, 1));
-	else {
-		res.send(render(req, '편집요청', '편집요청', {}));
+	
+	var baserev;
+	var data = await curs.execute("select rev from history where title = ? and namespace = ? order by CAST(rev AS INTEGER) desc limit 1", [doc.title, doc.namespace]);
+	try {
+		baserev = data[0].rev;
+	} catch(e) {
+		baserev = 0;
 	}
+	
+	if(req.method == 'POST') {
+		// 'edit_requests': ['title', 'namespace', 'id', 'deleted', 'state', 'content', 'baserev', 'username', 'ismember', 'log', 'date', 'processor', 'processortype'],
+		
+		var data = await curs.execute("select id from edit_requests order by cast(id as integer) desc limit 1");
+		var id = 1;
+		if(data.length) id = Number(data[0].id) + 1;
+		await curs.execute("insert into edit_requests (title, namespace, id, state, content, baserev, username, ismember, log, date, processor, processortype, lastupdate) values (?, ?, ?, 'open', ?, ?, ?, ?, ?, ?, '', '', ?)", 
+														[doc.title, doc.namespace, id, req.body['text'] || '', baserev, ip_check(req), islogin(req) ? 'author' : 'ip', req.body['log'] || '', getTime(), getTime()]);
+		
+		return res.redirect('/edit_request/' + id);
+	}
+	
+	var rawContent = await curs.execute("select content from documents where title = ? and namespace = ?", [doc.title, doc.namespace]);
+	if(!rawContent[0]) rawContent = '';
+	else rawContent = rawContent[0].content;
+	
+	var content = `
+		<form method="post" id="editForm" enctype="multipart/form-data" data-title="${title}" data-recaptcha="0">
+			<input type="hidden" name="token" value="">
+			<input type="hidden" name="identifier" value="${islogin(req) ? 'm' : 'i'}:${ip_check(req)}">
+			<input type="hidden" name="baserev" value="${baserev}">
+
+			<ul class="nav nav-tabs" role="tablist" style="height: 38px;">
+				<li class="nav-item">
+					<a class="nav-link active" data-toggle="tab" href="#edit" role="tab">편집</a>
+				</li>
+				<li class="nav-item">
+					<a id="previewLink" class="nav-link" data-toggle="tab" href="#preview" role="tab">미리보기</a>
+				</li>
+			</ul>
+
+			<div class="tab-content bordered">
+				<div class="tab-pane active" id="edit" role="tabpanel">
+					<textarea id="textInput" name="text" wrap="soft" class="form-control">${html.escape(rawContent)}</textarea>
+				</div>
+				<div class="tab-pane" id="preview" role="tabpanel">
+					
+				</div>
+			</div>
+			
+			<div class="form-group" style="margin-top: 1rem;">
+				<label class="control-label" for="summaryInput">요약</label>
+				<input type="text" class="form-control" id="logInput" name="log" value="">
+			</div>
+
+			<label><input type="checkbox" name="agree" id="agreeCheckbox" value="Y">&nbsp;문서 편집을 <strong>저장</strong>하면 당신은 기여한 내용을 <strong>CC-BY-NC-SA 2.0 KR</strong>으로 배포하고 기여한 문서에 대한 하이퍼링크나 URL을 이용하여 저작자 표시를 하는 것으로 충분하다는 데 동의하는 것입니다. 이 <strong>동의는 철회할 수 없습니다.</strong></label>
+			
+			${islogin(req) ? '' : `<p style="font-weight: bold;">비로그인 상태로 편집합니다. 편집 역사에 IP(${ip_check(req)})가 영구히 기록됩니다.</p>`}
+			
+			<div class="btns">
+				<button id="editBtn" class="btn btn-primary" style="width: 100px;">저장</button>
+			</div>
+		</form>
+	`;
+	
+	res.send(render(req, doc + ' (편집 요청)', content, {
+		document: doc,
+	}, '', _, 'new_edit_request'));
 });
 
 wiki.all(/^\/acl\/(.*)$/, async(req, res, next) => {
@@ -1657,8 +1844,9 @@ wiki.get(/^\/history\/(.*)/, async function viewHistory(req, res) {
 	const until = req.query['until'];
 	title = totitle(doc.title, doc.namespace);
 	
-	if(!await getacl(req, doc.title, doc.namespace, 'read')) {
-		return res.send(showError(req, 'insufficient_privileges_read'));
+	var aclmsg = await getacl(req, doc.title, doc.namespace, 'read', 1);
+	if(aclmsg) {
+		return res.send(showError(req, aclmsg, 1));
 	}
 	
 	var data;
@@ -3238,9 +3426,9 @@ wiki.use(function(req, res, next) {
 		else
 			permlist[prm['username']].push(prm['perm']);
 	}
+	
+	const server = wiki.listen(hostconfig['port'], hostconfig['host']);  // 서버실행
+	print(String(hostconfig['host']) + ":" + String(hostconfig['port']) + "에 실행 중. . .");
 })();
-
-const server = wiki.listen(hostconfig['port'], hostconfig['host']);  // 서버실행
-print(String(hostconfig['host']) + ":" + String(hostconfig['port']) + "에 실행 중. . .");
 
 }
