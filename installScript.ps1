473790f26bc8eadc8d5f0b2be56499d9d0a43df6
Dohyeon Jeon <jdh59681@naver.com>
2022-10-13 14:50:18 +0900

    Create installScript.ps1

---
+++ installScript.ps1
@@ -0,0 +1,10 @@
+# define
+$file = "js/theseed.js", "js/jquery-2.1.4.min.js", "js/jquery-1.11.3.min.js", "js/intersection-observer.js", "js/dateformatter.js", "css/wiki.css", "css/katex.min.css", "css/diffview.css"
+
+# wget -Uri $source -OutFile $relDir
+
+foreach ($aaa in $file) {
+    $source = "https://theseed.io/" + $aaa
+    $dest = $PSScriptRoot + "/" + $aaa
+    wget -Url $source -OutFile $dest
+}
