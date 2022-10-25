e3fa12be0729b43d588056afa864c7daa0883973
Dohyeon Jeon <jdh59681@naver.com>
2022-10-25 12:11:20 +0900

    versionhandler.js 삭제 외부에서 import 쓰는 게 막혀있을줄은..

--- server.js
+++ server.js
@@ -36,7 +36,18 @@ const wiki = express();  // 서버
 const conn = new sqlite3.Database('./wikidata.db', () => 0);  // 데이타베이스
 const upload = multer();  // 파일 올리기 모듈
 
-import 'js/versionhandler.js';
+// 위키의 마지막 커밋 날짜
+const wiki_version = "20220917";
+
+// 중요: 아래 3개의 기본값은 false, '', ''입니다.
+// 하나라도 기본값이면 수정 안 된 걸로 칩니다.
+// const라 정의 안하시면 안 되십니다.
+// 위키가 수정됨?
+const wiki_edited_is = true;
+// 위키를 수정한 자
+const wiki_edited_who = "JeonDohyeon";
+// 위키 마지막 수정 날짜
+const wiki_edited_when = "2022.10.25";
 
 var wikiconfig = {};  // 위키 설정 캐시
 var permlist = {};  // 권한 캐시
