21cb2a8fd45ea601dea743beed7b791d3fd546cf
Dohyeon Jeon <jdh59681@naver.com>
2022-10-25 12:05:52 +0900

    버전 핸들러 추가하고 풀 라이선스 구문 수정 솔직히 기존 구문보다 얘가 램 덜 먹을 거임. (boolean ? retWhenTrue : retWhenFalse)로 처리하는 거라. 참고로 풀 라이선스 쟤는 major, minor, revision 원래 받아오게 했었는데 귀찮음. 싫음 걍. 참고로 잘 안 될 가능성도 있음. 난 몰라 아직 안돌려봐서 버전 핸들러는 import "path-to-file"로 추가함.

---
+++ js/versionhandler.js
@@ -0,0 +1,12 @@
+// 위키의 마지막 커밋 날짜
+const wiki_version = "20220917";
+
+// 중요: 아래 3개의 기본값은 false, '', ''입니다.
+// 하나라도 기본값이면 수정 안 된 걸로 칩니다.
+// const라 정의 안하시면 안 되십니다.
+// 위키가 수정됨?
+const wiki_edited_is = true;
+// 위키를 수정한 자
+const wiki_edited_who = "JeonDohyeon";
+// 위키 마지막 수정 날짜
+const wiki_edited_when = "2022.10.25";
