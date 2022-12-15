2f6f58ce52ec732200676874048976b9187a3847
cadipox <120466862+cadipox@users.noreply.github.com>
2022-12-15 21:42:41 +0900

    sha224 package

--- routes/router.js
+++ routes/router.js
@@ -14,6 +14,7 @@ const diff = require('../cemerick-jsdifflib.js');
 const cookieParser = require('cookie-parser');
 const child_process = require('child_process');
 const captchapng = require('captchapng');
+const sha224 = require('sha224');
 
 const express = require('express');
 const router = express.Router();
