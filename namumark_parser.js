abc9d1baa9fb3bd0570753c04bbb26a16b8525e2
gdl-blue <55873929+gdl-blue@users.noreply.github.com>
2023-07-05 07:50:45 +0000

    #73 - 1번

--- namumark_parser.js
+++ namumark_parser.js
@@ -924,7 +924,7 @@ module.exports = async function markdown(req, content, discussion = 0, title = '
 		if(blockdata) {
 			data = `
 				<div style="border-width: 5px 1px 1px; border-style: solid; border-color: red gray gray; padding: 10px; margin-bottom: 10px;" onmouseover="this.style.borderTopColor=\'blue\';" onmouseout="this.style.borderTopColor=\'red\';">
-					<span style="font-size: 14pt;">이 사용자는 차단된 사용자입니다.${ver('4.18.0') ? ` (#${blockdata.id})` : ''}</span><br /><br />
+					<span${ver('4.13.0') ? '' : ' style="font-size: 14pt;"'}>이 사용자는 차단된 사용자입니다.${ver('4.18.0') ? ` (#${blockdata.id})` : ''}</span><br /><br />
 					이 사용자는 ${generateTime(toDate(blockdata.date), timeFormat)}에 ${blockdata.expiration == '0' ? '영구적으로' : (generateTime(toDate(blockdata.expiration), timeFormat) + '까지')} 차단되었습니다.<br />
 					차단 사유: ${html.escape(blockdata.note)}
 				</div>
@@ -960,7 +960,7 @@ module.exports = async function markdown(req, content, discussion = 0, title = '
 			} else if(getperm('admin', doc.title)) {
 				data = `
 					<div style="border-width: 5px 1px 1px; border-style: solid; border-color: orange gray gray; padding: 10px; margin-bottom: 10px;" onmouseover="this.style.borderTopColor=\'red\';" onmouseout="this.style.borderTopColor=\'orange\';">
-						<span style="font-size:14pt">이 사용자는 특수 권한을 가지고 있습니다.</span>
+						<span${ver('4.13.0') ? '' : ' style="font-size: 14pt;"'}>이 사용자는 특수 권한을 가지고 있습니다.</span>
 					</div>
 				` + data;
 			}
